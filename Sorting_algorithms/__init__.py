2d9d9df477c34d32e736ac877a44a1622b0360f7
lamkhanhtran <tranphamlamkhanh@gmail.com>
2025-01-31 15:37:21 +0900

    Remove miracle sort

--- Sorting_algorithms/__init__.py
+++ Sorting_algorithms/__init__.py
@@ -4,5 +4,4 @@ from .merge_sort import merge_sort
 from .insertion_sort import insertion_sort
 from .selection_sort import selection_sort
 from .radix_sort import radix_sort
-from .miracle_sort import miracle_sort
 from .heap_sort import heap_sort
